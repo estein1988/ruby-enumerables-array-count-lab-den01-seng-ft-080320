bed822bc61d01f2c764ce61dd6b534725f69baff
Elliott Stein <estein1988@gmail.com>
2020-07-13 17:32:05 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -3,6 +3,7 @@ array = [1, "hello", [], 5.01, "world", :name, { a: 1 }]
 def count_strings(array)
   array.count do |element|
     element.class == String?
+  end
 end
 
 def count_empty_strings(array)
